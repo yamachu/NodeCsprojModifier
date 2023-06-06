13a20f46fdee7396c1b7f094580e085730cfa010
yamachu <yamachu.dev@gmail.com>
2023-06-07 00:55:34 +0900

    Add 1-space...

--- src/Constants.ts
+++ src/Constants.ts
@@ -6,7 +6,7 @@ export const CSPROJ_PROPERTY_TargetFramework = "TargetFramework";
 export const EXTENSION_CONFIGURE_FILE_NAME = "TargetFrameworkSwitcher.targets";
 export const CSPROJ_PROPERTY_EXTENSION_CONFIGURE_FILE_NAME = `$(MSBuildProjectDirectory)\\${EXTENSION_CONFIGURE_FILE_NAME}`;
 
-export const CSPROJ_PROPERTY_Import_Tag = `   <!-- NOTE: TargetFrameworkSwitcher inserts it, DO NOT EDIT YOURSELF and IT REQUIRES TOP-LEVEL EVALUATION BEFORE USING "TargetFramework" VARIABLE -->
+export const CSPROJ_PROPERTY_Import_Tag = `    <!-- NOTE: TargetFrameworkSwitcher inserts it, DO NOT EDIT YOURSELF and IT REQUIRES TOP-LEVEL EVALUATION BEFORE USING "TargetFramework" VARIABLE -->
     <Import Project="${CSPROJ_PROPERTY_EXTENSION_CONFIGURE_FILE_NAME}" Condition="Exists('${CSPROJ_PROPERTY_EXTENSION_CONFIGURE_FILE_NAME}')" />
 `;
 
